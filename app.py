96e8c168dac009f367568b1213bed6311802f98d
Aleia Knight <aleiaknight@gmail.com>
2019-10-01 08:41:42 -0700

    making cchanges

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
 
-client = MongoClient()
+
+# client = MongoClient()
+# db = client.Playlister
+# playlists = db.playlists
+
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.Playlister
 playlists = db.playlists
 
@@ -68,4 +76,4 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
     
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
